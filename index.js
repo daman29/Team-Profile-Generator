aa0fbcf260435faa3a3d66bd39e2315003888134
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-19 22:13:05 +0930

    added full prompt interface with list selection and logic to keep building team until finish

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const { type } = require("os");
 
 const employees = [];
 
@@ -12,7 +13,7 @@ inquirer
     {
       type: "input",
       name: "managerName",
-      message: "Please enter the manager's name",
+      message: "Please enter the manager's name: ",
       validate: function (input) {
         if (!input) {
           console.log("Please enter a valid name");
@@ -24,7 +25,7 @@ inquirer
     {
       type: "input",
       name: "managerId",
-      message: "Please enter the manager's Id",
+      message: "Please enter the manager's Id: ",
       validate: function (input) {
         if (!input) {
           console.log("Please enter a valid Id");
@@ -36,12 +37,12 @@ inquirer
     {
       type: "input",
       name: "managerEmail",
-      message: "Please enter the manager's Email",
+      message: "Please enter the manager's Email: ",
     },
     {
       type: "number",
       name: "officeNumber",
-      message: "Please enter the office number",
+      message: "Please enter the office number: ",
       validate: function (input) {
         if (isNaN(input)) {
           console.log(
@@ -65,14 +66,206 @@ inquirer
 
     employeeMenu();
   })
-  .catch((error) => { console.log(error)});
+  .catch((error) => {
+    console.log(error);
+  });
 
 const employeeMenu = () => {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "type",
-            message: "Add another employee or finish the team? "
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "type",
+        message: "Add another employee or finish the team?",
+        choices: ["Engineer", "Intern", "Finish"],
+      },
     ])
-}
+    .then((response) => {
+      if (response.type === "Finish") {
+        console.log("team finished");
+        console.log(employees);
+      } else {
+        createEmployee(response.type);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// const createEmployee = (type) => {
+//   if (type === "Engineer") {
+//     return inquirer
+//       .prompt([
+//         {
+//           type: "input",
+//           name: "name",
+//           message: "Please enter the employees's name",
+//           validate: function (input) {
+//             if (!input) {
+//               console.log("Please enter a valid name");
+//             } else {
+//               return true;
+//             }
+//           },
+//         },
+//         {
+//           type: "input",
+//           name: "id",
+//           message: "Please enter the employee's Id",
+//           validate: function (input) {
+//             if (!input) {
+//               console.log("Please enter a valid Id");
+//             } else {
+//               return true;
+//             }
+//           },
+//         },
+//         {
+//           type: "input",
+//           name: "email",
+//           message: "Please enter the employee's Email",
+//         },
+//         {
+//           type: "input",
+//           name: "github",
+//           message: "Please enter their GitHub username",
+//         },
+//       ])
+//       .then((response) => {
+//         const engineer = new Engineer(
+//           response.name,
+//           response.id,
+//           response.email,
+//           response.github
+//         );
+
+//         employees.push(engineer);
+
+//         employeeMenu();
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   } else if (type === "Intern") {
+//     return inquirer
+//       .prompt([
+//         {
+//           type: "input",
+//           name: "name",
+//           message: "Please enter the employees's name",
+//           validate: function (input) {
+//             if (!input) {
+//               console.log("Please enter a valid name");
+//             } else {
+//               return true;
+//             }
+//           },
+//         },
+//         {
+//           type: "input",
+//           name: "id",
+//           message: "Please enter the employee's Id",
+//           validate: function (input) {
+//             if (!input) {
+//               console.log("Please enter a valid Id");
+//             } else {
+//               return true;
+//             }
+//           },
+//         },
+//         {
+//           type: "input",
+//           name: "email",
+//           message: "Please enter the employee's Email",
+//         },
+//         {
+//           type: "input",
+//           name: "school",
+//           message: "Please enter the school name",
+//         },
+//       ])
+//       .then((response) => {
+//         const intern = new Intern(
+//           response.name,
+//           response.id,
+//           response.email,
+//           response.school
+//         );
+
+//         employees.push(intern);
+
+//         employeeMenu();
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+// };
+
+const createEmployee = (typeEm) => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the employees's name: ",
+        validate: function (input) {
+          if (!input) {
+            console.log("Please enter a valid name");
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the employee's Id: ",
+        validate: function (input) {
+          if (!input) {
+            console.log("Please enter a valid Id");
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the employee's Email: ",
+      },
+    ])
+    .then((response) => {
+      if (typeEm === "Engineer") {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "github",
+            message: "Please enter their GitHub username: ",
+          },
+        ]).then((result) => {            
+            const engineer = new Engineer(response.name, response.id, response.email, result)
+
+            employees.push(engineer)
+            employeeMenu();
+        });
+      } else {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "school",
+            message: "Please enter their school name: ",
+          },
+        ]).then(result => {
+            const intern = new Intern(response.name, response.id, response.email, result)
+
+            employees.push(intern)
+            employeeMenu();
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
