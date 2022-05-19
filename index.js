399a3fc54ce841f7f16a59954fc2dfbf04b7ba87
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-19 20:52:33 +0930

    added basic structure and test to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+console.log(new Manager('David', 2, 'david@email.com', 35))
+console.log(new Engineer('David', 4, 'david@email.com', 'daman29'))
+console.log(new Intern('David', 6, 'david@email.com', 'Adelaide Uni'))
