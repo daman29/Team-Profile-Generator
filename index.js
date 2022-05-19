de656280a64a90a838c88c6705fb664a3a7a1449
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-19 21:31:41 +0930

    added start inquirer prompts for manager and starting employee selection prompt

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-console.log(new Manager('David', 2, 'david@email.com', 35))
-console.log(new Engineer('David', 4, 'david@email.com', 'daman29'))
-console.log(new Intern('David', 6, 'david@email.com', 'Adelaide Uni'))
+const employees = [];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "Please enter the manager's name",
+      validate: function (input) {
+        if (!input) {
+          console.log("Please enter a valid name");
+        } else {
+          return true;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "Please enter the manager's Id",
+      validate: function (input) {
+        if (!input) {
+          console.log("Please enter a valid Id");
+        } else {
+          return true;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "Please enter the manager's Email",
+    },
+    {
+      type: "number",
+      name: "officeNumber",
+      message: "Please enter the office number",
+      validate: function (input) {
+        if (isNaN(input)) {
+          console.log(
+            "Please enter a valid Office Number, Press up arrow to change input"
+          );
+        } else {
+          return true;
+        }
+      },
+    },
+  ])
+  .then((response) => {
+    const manager = new Manager(
+      response.managerName,
+      response.managerId,
+      response.managerEmail,
+      response.officeNumber
+    );
+
+    employees.push(manager);
+
+    employeeMenu();
+  })
+  .catch((error) => { console.log(error)});
+
+const employeeMenu = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "Add another employee or finish the team? "
+        }
+    ])
+}
